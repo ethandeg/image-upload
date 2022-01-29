c3f9ef13a9bf12a27fd5b57bfeed8f8c335ddab3
Ethan Degenhardt <ethandeg1996@gmail.com>
2022-01-29 17:50:26 -0600

    set up boilerplate and store

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import {createStore} from 'vuex';
+// import Vue from 'vue';
+
+// Vue.use(Vuex);
+
+export const store = createStore({
+    state(){
+        return {
+            modules: {},
+        }
+    }
+})
